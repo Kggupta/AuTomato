7dbe0f234410bfaf7455cb7da1baa60774015bbb
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-01-19 18:35:57 -0500

    file position class

---
+++ include/Position.h
@@ -0,0 +1,25 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+#include <string>
+#include <utility>
+
+using namespace std;
+
+class Position {
+    public:
+        Position() = default;;
+        Position(int current, int line, int column);
+        Position(int current, int line, int column, string file, string fcontent);
+        Position* advance(char current);
+        Position copy();
+
+        int position = 0;
+        int line = 0;
+        int column = 0;
+
+        string file;
+        string fcontent;
+};
+
+#endif
---
+++ src/Position.cc
@@ -0,0 +1,24 @@
+#include "Position.h"
+
+using namespace std;
+
+Position::Position(int cur, int line, int column) : position(cur), line(line), column(column) {};
+
+Position::Position(int cur, int line, int column, string file, string fcontent) 
+                : position(cur), line(line), column(column), file(move(file)), fcontent(move(fcontent)) {};
+
+Position * Position::advance(char current) {
+    position++;
+    column++;
+
+    if (current == '\n') {
+        line++;
+        column = 0;
+    }
+
+    return this;
+}
+
+Position Position::copy() {
+    return Position(position, line, column, file, fcontent);
+}
