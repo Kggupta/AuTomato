26573f39b88ae573f2846b2f52d995c27d9bef99
Keshav Gupta <33162944+Kggupta@users.noreply.github.com>
2023-01-20 17:42:14 -0500

    add operation precedence

---
+++ include/ParseHelper.h
@@ -0,0 +1,34 @@
+#ifndef PARSER_HELPER_H
+#define PARSER_HELEPR_H
+
+#include "Token.h"
+
+#include <memory>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+enum Precedence {
+    ASSIGNMENT = 1,
+    CONDITION = 2,
+    OR = 4,
+    AND = 5,
+    BOR = 6,
+    XOR = 7,
+    BAND = 8,
+    EQUALITY = 9,
+    INEQUALITY = 10,
+    SHIFT = 11,
+    SUM = 12, 
+    PRODUCT = 13,
+    EXPONENT = 14, 
+    PREFIX = 14,
+    POSTFIX = 15,
+    INDEX = 15,
+    CALL = 15
+};
+
+int getTokenPrecedence(const Token& tok);
+
+#endif
